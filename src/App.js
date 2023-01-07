850b83c7397c6493ee798c415326ee90ed6fc6a4
MaxT6 <maximiliant6@gmail.com>
2023-01-07 12:14:57 -0500

    Testing deployment

--- src/App.js
+++ src/App.js
@@ -3,21 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Initializing Repo
-        </a>
-      </header>
+    <div>
+      <h1>Maximilian Thiel's website is up</h1>
     </div>
   );
 }
